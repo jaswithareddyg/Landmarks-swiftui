5dbf840d373514853bf30f894d5712a0274add1e
Jas <jaswithareddy@uchicago.edu>
2023-02-12 23:41:54 -0600

    Commit - 3

--- Landmarks/Landmarks/CircleImage.swift
+++ Landmarks/Landmarks/CircleImage.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Image("turtlerock")
+        Image("shawnee")
+            .resizable()
+            .frame(width: 250, height: 250)
             .clipShape(Circle())
             .overlay{
                 Circle().stroke(.white, lineWidth: 4)
